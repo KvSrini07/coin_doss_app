e48fd15ee0b601fb78f19363cc02993114f53748
Srinivasan K V <kvsbsc081@gmail.com>
2025-11-22 13:38:13 +0530

    Update db

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,29 +1,58 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from app.core.config import settings
+# from app.models.tables import Base
+
+# engine = create_engine(
+#     settings.DATABASE_URL,
+#     pool_pre_ping=True
+# )
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# # # Only for SQLit
+# # engine = create_engine(
+# #     settings.DATABASE_URL, connect_args={"check_same_thread": False}  # For SQLite
+# # )
+# # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# def init_db():
+#     Base.metadata.create_all(bind=engine)
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+import os
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.core.config import settings
-from app.models.tables import Base
+
+DB_USER = os.getenv("MYSQLUSER")
+DB_PASS = os.getenv("MYSQLPASSWORD")
+DB_HOST = os.getenv("MYSQLHOST")
+DB_PORT = os.getenv("MYSQLPORT")
+DB_NAME = os.getenv("MYSQLDATABASE")
+
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 engine = create_engine(
-    settings.DATABASE_URL,
-    pool_pre_ping=True
+    DATABASE_URL,
+    pool_pre_ping=True,     # avoids stale connection issues
+    pool_recycle=280        # prevents idle timeout on Railway
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# # Only for SQLit
-# engine = create_engine(
-#     settings.DATABASE_URL, connect_args={"check_same_thread": False}  # For SQLite
-# )
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
 
 def init_db():
+    from app.models.tables import Base
     Base.metadata.create_all(bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
